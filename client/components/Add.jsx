04cb78af07b57cf30e858ba3d3066981ec52d36c
Philip Chan <filnzl@hotmail.com>
2020-11-23 21:24:09 +1300

    add space for hadnelsbmit on add form

--- client/components/Add.jsx
+++ client/components/Add.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import { HashRouter as Router, Route, Link } from 'react-router-dom'
-import { connect } from 'react-redux'
+import React from "react";
+import { HashRouter as Router, Route, Link } from "react-router-dom";
+import { connect } from "react-redux";
 // import bulmaCalendar from '~bulma-calendar/dist/js/bulma-calendar.min.js';
 
 // import { checkAuth } from '../actions/auth'
@@ -18,31 +18,43 @@ import { connect } from 'react-redux'
 // }
 
 export class Add extends React.Component {
-  componentDidMount() {
-  }
+  componentDidMount() {}
+
+  handleSubmit = (e) => {
+      
+  };
 
   render() {
     return (
       <>
-        <div className="field">
-          <label className="label">Event Name</label>
-          <div className="control">
-            <input className="input" type="text" placeholder="e.g Dinner" />
+        <form className="form box" onSubmit={this.handleSubmit}>
+          <div className="field">
+            <label className="label">Event Name</label>
+            <div className="control">
+              <input className="input" type="text" placeholder="e.g Dinner" />
+            </div>
           </div>
-        </div>
 
-        <div className="field">
-          <label className="label">Description</label>
-          <div className="control">
-            <input className="input" type="email" placeholder="e.g. with friends" />
+          <div className="field">
+            <label className="label">Description</label>
+            <div className="control">
+              <input
+                className="input"
+                type="email"
+                placeholder="e.g. with friends"
+              />
+            </div>
           </div>
-        </div>
 
-        <input type="date" />
-
+          <div className="field">
+            <label className="label">Date</label>
+            <div className="control">
+              <input type="date" />
+            </div>
+          </div>
+        </form>
       </>
-
-    )
+    );
   }
 }
-export default Add
+export default Add;
