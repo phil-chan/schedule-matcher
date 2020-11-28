db9a9f4e005dd402a2fc5c7d1f00ef6c1d887edb
philc <32604473+Phil160@users.noreply.github.com>
2020-11-28 17:34:40 +1300

    update seed to generate hash and password

--- seeds/users.js
+++ seeds/users.js
@@ -1,13 +1,39 @@
+const { generateHash } = require("authenticare/server");
+
 exports.seed = function (knex) {
   // Deletes ALL existing entries
   return knex("users")
     .del()
     .then(function () {
-      // Inserts seed entries
-      return knex("users").insert([
-        { id: 1, username: "phil", email: "filnzl@hotmail.com" },
-        { id: 2, username: "peter", email: "" },
-        { id: 3, username: "ponyboy", email: "" },
-      ]);
+      return Promise.all(
+        [
+          {
+            id: 1,
+            username: "phil",
+            password: "password",
+            email: "filnzl@hotmail.com"
+          },
+          {
+            id: 2,
+            username: "bill",
+            password: "password",
+            email: ""
+          },
+          {
+            id: 3,
+            username: "mill",
+            password: "password",
+            email: ""
+          }
+        ].map((user) => {
+          return generateHash(user.password).then((hash) => {
+            user.hash = hash;
+            delete user.password;
+            return user;
+          });
+        })
+      ).then((users) => {
+        return knex("users").insert(users);
+      });
     });
 };
